a264cc771942d7d49ccb0e9c66d3e8ae0d9e8f6b
good4ram@naver.com <good4ram@naver.com>
2024-08-13 18:22:39 +0900

    this is java repository start

---
+++ thisisjava/src/ch04/Example07.java
@@ -0,0 +1,43 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class Example07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int choiceNum;
+        int balance=0;
+
+        while(true) {
+            System.out.println("-------------------------------------");
+            System.out.println("1: 예금 | 2. 출금 | 3.잔고 | 4.종료");
+            System.out.println("-------------------------------------");
+
+            System.out.print("선택> ");
+
+            choiceNum = input.nextInt();
+
+            switch(choiceNum) {
+                case 1:
+                    System.out.print("예금액> ");
+                    balance += input.nextInt();
+                    break;
+                case 2:
+                    System.out.print("출금액> ");
+                    balance -= input.nextInt();
+                    break;
+                case 3:
+                    System.out.println("잔고> "+balance);
+                    break;
+            }
+
+            if(choiceNum == 4) {
+                System.out.println("프로그램 종료");
+                break;
+            }
+        }
+
+        input.close();
+    }
+}
