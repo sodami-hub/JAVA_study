8fbcb125f770d4148bb8d8c4e284df7bea66f0a2
good4ram@naver.com <good4ram@naver.com>
2024-08-27 21:24:42 +0900

    'DS&Algorithm' Using Arrays.binarySearch method.

--- algorithm/src/ch03_searchAlgorithm/Q05.java
+++ algorithm/src/ch03_searchAlgorithm/Q05.java
@@ -1,5 +1,7 @@
 package ch03_searchAlgorithm;
 
+import java.util.Arrays;
+
 // 이진 탐색 에서 같은 크기의 값이 나란히 있을 때 가장 앞쪽에 있는 요소를 반환하도록 한다.
 public class Q05 {
 
@@ -30,10 +32,14 @@ public class Q05 {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,7,7,7,7,7,7,8,8,9,9};
+        int[] arr = {1,2,3,5,7,7,7,8,8,9,9};
         int n = arr.length;
         int index = binSearchX(arr, n, 7);
 
+        System.out.println(index);
+
+        // Arrays.binarySearch 메서드 사용해보기.
+        index = Arrays.binarySearch(arr,6);
         System.out.println(index);
     }
 }
