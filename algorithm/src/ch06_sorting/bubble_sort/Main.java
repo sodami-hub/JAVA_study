9505ed5dd6419e8989b4acbf5b406806009d75dd
good4ram@naver.com <good4ram@naver.com>
2024-09-02 17:45:11 +0900

    simple modify

--- algorithm/src/ch06_sorting/bubble_sort/Main.java
+++ algorithm/src/ch06_sorting/bubble_sort/Main.java
@@ -2,6 +2,7 @@ package ch06_sorting.bubble_sort;
 
 import java.util.Arrays;
 
+// 각 정렬 버전별 반복 및 비교횟수 비교.
 public class Main {
     public static void main(String[] args) {
         System.out.println("\nversion 1 =======================================================\n");
@@ -17,5 +18,8 @@ public class Main {
         int[] arr3 = new int[] {1,3,9,4,7,8,6,54,246,12566,323,34,2667,2,4667,2,89,56734,25656,132,1,4686};
         BubbleSortV03.bubbleSortV03(arr3, arr3.length);
         System.out.println(Arrays.toString(arr3));
+
+        int[] arr4 = {1,3,9,4,7,8,6,54,246,12566,323,34,2667,2,4667,2,89,56734,25656,132,1,4686};
+        ShakerSort.shakerSort(arr4,arr4.length);
     }
 }
