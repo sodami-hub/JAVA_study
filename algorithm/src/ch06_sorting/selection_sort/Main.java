0533111e2bc4140aeb0c17c3b09b8e7fae1702ca
good4ram@naver.com <good4ram@naver.com>
2024-09-03 11:17:02 +0900

    'Algorithm' ch06 Q06 Add viewSelectionSort method

--- algorithm/src/ch06_sorting/selection_sort/Main.java
+++ algorithm/src/ch06_sorting/selection_sort/Main.java
@@ -5,9 +5,15 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //단순선택정렬
+
         int[] arr = {6,4,8,3,1,9,7};
         StraightSelectionSort sss = new StraightSelectionSort();
         sss.selectionSort(arr,arr.length);
+//        System.out.println("arr = " + Arrays.toString(arr));
+
+        //단순선택정렬 상세 보여주기 Q6 sol
+        int[] arr01 = {6,4,8,3,1,9,7};
+        sss.viewSelectionSort(arr01,arr01.length);
         System.out.println("arr = " + Arrays.toString(arr));
     }
 }
--- algorithm/src/ch06_sorting/selection_sort/StraightSelectionSort.java
+++ algorithm/src/ch06_sorting/selection_sort/StraightSelectionSort.java
@@ -14,4 +14,41 @@ public class StraightSelectionSort {
             arr[minIndex] = temp;
         }
     }
+
+    //Q6 -- 단순 선택 정렬의 동작과정을 상세하게 보여주기.
+    public void viewSelectionSort(int[] arr, int len) {
+        for(int i = 0; i < len-1; i++) {
+            int minIndex = i;
+
+            for(int k=0; k<=i; k++) {
+                if(k==i) {
+                    System.out.print("  *");
+                    break;
+                }
+                System.out.print("   ");
+            }
+
+
+            for(int j= i+1; j<len; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            for(int k= i+1; k<=minIndex; k++) {
+                if(k==minIndex) {
+                    System.out.print("  +");
+                    System.out.println();
+                    break;
+                }
+                System.out.print("   ");
+            }
+            for(int num : arr) {
+                System.out.printf("%3d",num);
+            }
+            System.out.println();
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
 }
