5462aae0235a987850f0641812d22ff7639d2bfa
good4ram@naver.com <good4ram@naver.com>
2024-09-04 14:05:01 +0900

    'Algorithm' ch06 Add Shell_Sort Version02 + Q10

--- algorithm/src/ch06_sorting/shell_sort/Main.java
+++ algorithm/src/ch06_sorting/shell_sort/Main.java
@@ -6,10 +6,15 @@ public class Main {
     public static void main(String[] args) {
 
         //Shell Sort Version01
-        int[] arr = {8, 1, 4, 2, 7, 6, 3};
+        int[] arr = {4756,241,5677,231,54,1668,1457,34,82,2,66,898956,134656,2345,4,23,65,1,7,3,1,43,98,6,66789,1,578,1234,4787,125,6,23,8000,54784,1247798,24,667};
 
-        ShellSortV01 ss = new ShellSortV01();
-        ss.shellSort(arr, arr.length);
+        ShellSort ss = new ShellSort();
+        ss.shellSortV01(arr, arr.length);
         System.out.println(Arrays.toString(arr));
+
+        //Shell Sort Version02
+        int[] arr02 = {4756,241,5677,231,54,1668,1457,34,82,2,66,898956,134656,2345,4,23,65,1,7,3,1,43,98,6,66789,1,578,1234,4787,125,6,23,8000,54784,1247798,24,667};
+        ss.shellSortV02(arr02,arr02.length);
+        System.out.println(Arrays.toString(arr02));
     }
 }
---
+++ algorithm/src/ch06_sorting/shell_sort/ShellSort.java
@@ -0,0 +1,60 @@
+package ch06_sorting.shell_sort;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static java.lang.System.currentTimeMillis;
+
+public class ShellSort {
+
+    // Shell Sort version01
+    public void shellSortV01(int[] a, int n) {
+        long start = currentTimeMillis();
+        int h = n;
+        int count=0;
+        while (true) {
+            h /= 2;
+            System.out.println("h : "+h);
+            for (int i = 0; i < n-h; i++) {
+                for (int j = 0; j < n-h; j++) {
+                    if(a[j]> a[j+h]) {
+                        int tmp = a[j];
+                        a[j] = a[j + h];
+                        a[j + h] = tmp;
+                        count++;
+                    }
+                }
+            }
+            if (h == 1) break;
+        }
+        System.out.println("v01 소요시간 : "+(currentTimeMillis()-start));
+        System.out.println("교환 횟수 :" + count);
+    }
+
+    // Shell Sort version02
+    public void shellSortV02(int[] a, int n) {
+        long start = currentTimeMillis();
+        int h=0;
+        int count=0;
+        for(int i=1;i<n;i=i*3+1){
+            h=i;
+        }
+
+        for(; h>=1; h/=3) {
+            System.out.println("h : "+h);
+            for(int i =0; i<n-h; i++) {
+                for(int j=0; j<n-h; j++) {
+                    if(a[j]> a[j+h]) {
+                        int tmp = a[j];
+                        a[j] = a[j + h];
+                        a[j + h] = tmp;
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println("v02 소요시간 : "+(currentTimeMillis()-start));
+        System.out.println("교환 횟수 :" + count);
+    }
+
+}
