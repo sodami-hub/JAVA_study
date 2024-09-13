59be7d93d1f5b1854c30ec6a9266a664e047f602
good4ram@naver.com <good4ram@naver.com>
2024-09-14 03:31:16 +0900

    'codingtest' Q02 sol

---
+++ codingtest_java/src/Test02.java
@@ -0,0 +1,56 @@
+/*
+문제 02 - 배열 제어하기
+중복값 제거, 내림차순 정렬
+*/
+
+
+import java.util.*;
+
+public class Test02 {
+
+    static Comparator<Integer> comp = new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            if(o1>o2) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+    };
+
+    public static void sol01(int[] arr) {
+        //Set 에 넣어서 중복값 제거
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i = 0; i<arr.length; i++) {
+            hs.add(arr[i]);
+        }
+
+        //Set에서 다시 Array로 카피
+        ArrayList<Integer> res = new ArrayList<>();
+        for (Integer h : hs) {
+            res.add(h);
+        }
+
+        //Comparator 객체로 내림차순 정렬
+        res.sort(comp);
+
+        for (Integer re : res) {
+            System.out.print(re+" ");
+        }
+    }
+
+    public static int[] sol02(int[] arr) {
+        Integer[] temp = Arrays.stream(arr).distinct().boxed().toArray(Integer[]::new);
+        Arrays.sort(temp, Collections.reverseOrder());
+        int[] array = Arrays.stream(temp).mapToInt(Integer::intValue).toArray();
+        System.out.println(Arrays.toString(array));
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,1,1,3,2,5,4};
+
+        int[] array = sol02(arr);
+    }
+}
